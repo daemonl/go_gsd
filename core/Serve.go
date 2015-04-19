91bd78fd58e2d2f5a7e8a3501d7ea4f952820334
Damien Whitten <damien@rebase.com.au>
2015-04-19 15:09:03 +1000

    Generify Serve Methods for custom go apps

--- core/Serve.go
+++ core/Serve.go
@@ -13,6 +13,7 @@ import (
 	"github.com/daemonl/go_gsd/file"
 	"github.com/daemonl/go_gsd/minihandlers"
 	"github.com/daemonl/go_gsd/router"
+	"github.com/daemonl/go_gsd/shared"
 	"github.com/daemonl/go_gsd/socket"
 	"github.com/daemonl/go_gsd/torch"
 	"github.com/daemonl/go_lib/google_auth"
@@ -20,47 +21,73 @@ import (
 
 var re_unsafe *regexp.Regexp = regexp.MustCompile(`[^A-Za-z0-9]`)
 
+type Server struct {
+	router       router.Router
+	sessionStore shared.ISessionStore
+	core         *GSDCore
+	config       *ServerConfig
+}
+
+type IServer interface {
+	Serve() error
+	AddRoute(format string, handler shared.IHandler, methods ...string) error
+	AddRouteFunc(format string, handlerFunc func(shared.IRequest) (shared.IResponse, error), methods ...string) error
+	AddRoutePathFunc(format string, handlerPathFunc func(shared.IPathRequest) (shared.IResponse, error), methods ...string) error
+}
+
 // Serve starts up a http server with all of the glorious configuration options
 func Serve(config *ServerConfig) error {
-
-	core, err := config.GetCore()
+	server, err := BuildServer(config)
 	if err != nil {
 		return err
 	}
+	return server.Serve()
+}
 
-	model := core.GetModel()
+func BuildServer(config *ServerConfig) (IServer, error) {
 
-	lilo := torch.GetBasicLoginLogout(core.DB, "staff")
+	s := &Server{
+		config: config,
+	}
+	var err error
 
-	sessionStore := torch.InMemorySessionStore(config.SessionDumpFile, lilo.LoadUserById, core.OpenDatabaseConnection, core.CloseDatabaseConnection)
+	s.core, err = config.GetCore()
+	if err != nil {
+		return nil, err
+	}
 
-	parser := torch.BasicParser(sessionStore, config.PublicPatternsRaw)
+	lilo := torch.GetBasicLoginLogout(s.core.DB, "staff")
+
+	s.sessionStore = torch.InMemorySessionStore(config.SessionDumpFile, lilo.LoadUserById, s.core.OpenDatabaseConnection, s.core.CloseDatabaseConnection)
+
+	parser := torch.BasicParser(s.sessionStore, config.PublicPatternsRaw)
 
 	// Register the AJAX and Socket methods (These methods work on both)
 	handlerMap := map[string]actions.Handler{
-		"get":     &actions.SelectQuery{Core: core},
-		"set":     &actions.UpdateQuery{Core: core},
-		"create":  &actions.CreateQuery{Core: core},
-		"delete":  &actions.DeleteQuery{Core: core},
-		"custom":  &actions.CustomQuery{Core: core},
-		"dynamic": &actions.DynamicHandler{Core: core},
-		"ping":    &actions.PingAction{Core: core},
-		"msg":     &actions.MsgAction{Core: core},
+		"get":     &actions.SelectQuery{Core: s.core},
+		"set":     &actions.UpdateQuery{Core: s.core},
+		"create":  &actions.CreateQuery{Core: s.core},
+		"delete":  &actions.DeleteQuery{Core: s.core},
+		"custom":  &actions.CustomQuery{Core: s.core},
+		"dynamic": &actions.DynamicHandler{Core: s.core},
+		"ping":    &actions.PingAction{Core: s.core},
+		"msg":     &actions.MsgAction{Core: s.core},
 	}
 
 	socketManager := socket.GetManager(parser.Store)
-	routes := router.GetRouter(parser, config.PublicPatternsRaw)
+	s.router = router.GetRouter(parser, config.PublicPatternsRaw)
 
 	for funcName, handler := range handlerMap {
 		func(funcName string, handler actions.Handler) {
 			socketManager.RegisterHandler(funcName, handler)
-			routes.AddRoute("/ajax/"+funcName, actions.AsRouterHandler(handler), "POST")
+			s.router.AddRoute("/ajax/"+funcName, actions.AsRouterHandler(handler), "POST")
 		}(funcName, handler)
 	}
 
-	loginViewHandler := core.TemplateManager.GetSimpleTemplateHandler("login.html")
-	setPasswordViewHandler := core.TemplateManager.GetSimpleTemplateHandler("set_password.html")
+	loginViewHandler := s.core.TemplateManager.GetSimpleTemplateHandler("login.html")
+	setPasswordViewHandler := s.core.TemplateManager.GetSimpleTemplateHandler("set_password.html")
 
+	model := s.core.GetModel()
 	fileHandler := file.GetFileHandler(config.UploadDirectory, model)
 
 	if config.OAuthConfig != nil {
@@ -75,41 +102,59 @@ func Serve(config *ServerConfig) error {
 	// SET UP URLS
 
 	http.Handle("/socket", socketManager.GetListener())
-	http.HandleFunc("/script/", core.runScript)
+	http.HandleFunc("/script/", s.core.runScript)
 
 	if config.DevMode {
 		log.Println("---DEV MODE---")
-		routes.AddRoute("/app.html", &minihandlers.RawFileHandler{WebRoot: config.WebRoot, Filename: "app_dev.html"})
+		s.router.AddRoute("/app.html", &minihandlers.RawFileHandler{WebRoot: config.WebRoot, Filename: "app_dev.html"})
 		http.Handle("/main.css", &minihandlers.LessHandler{WebRoot: config.WebRoot, Filename: "less/main.less"})
 		http.Handle("/pdf.css", &minihandlers.LessHandler{WebRoot: config.WebRoot, Filename: "less/pdf.less"})
 	}
 
-	routes.AddRoute("/login", loginViewHandler, "GET")
-	routes.AddRouteFunc("/login", lilo.HandleLogin, "POST")
-	routes.AddRouteFunc("/logout", lilo.HandleLogout)
-	routes.AddRoute("/set_password", setPasswordViewHandler, "GET")
-	routes.AddRouteFunc("/set_password", lilo.HandleSetPassword, "POST")
+	s.router.AddRoute("/login", loginViewHandler, "GET")
+	s.router.AddRouteFunc("/login", lilo.HandleLogin, "POST")
+	s.router.AddRouteFunc("/logout", lilo.HandleLogout)
+	s.router.AddRoute("/set_password", setPasswordViewHandler, "GET")
+	s.router.AddRouteFunc("/set_password", lilo.HandleSetPassword, "POST")
 
-	routes.AddRoutePathFunc("/report_html/%s/%d", core.Reporter.Handle, "GET")
-	routes.AddRoutePathFunc("/report_pdf/%s/%d", core.PDFHandler.Handle, "GET")
+	s.router.AddRoutePathFunc("/report_html/%s/%d", s.core.Reporter.Handle, "GET")
+	s.router.AddRoutePathFunc("/report_pdf/%s/%d", s.core.PDFHandler.Handle, "GET")
 
-	routes.AddRoutePathFunc("/emailpreview/%s/%d", core.Reporter.Handle, "GET")
-	routes.AddRoutePathFunc("/sendmail/%s/%d/%s/%s", core.MailHandler.Handle)
+	s.router.AddRoutePathFunc("/emailpreview/%s/%d", s.core.Reporter.Handle, "GET")
+	s.router.AddRoutePathFunc("/sendmail/%s/%d/%s/%s", s.core.MailHandler.Handle)
 
-	routes.AddRoutePathFunc("/csv/%s", core.CSVHandler.Handle)
+	s.router.AddRoutePathFunc("/csv/%s", s.core.CSVHandler.Handle)
 
 	http.HandleFunc("/upload/", parser.Wrap(fileHandler.Upload))
 	http.HandleFunc("/download/", parser.Wrap(fileHandler.Download))
 
-	routes.Redirect("/", "app.html")
-	routes.Fallthrough(config.WebRoot)
+	s.router.Redirect("/", "app.html")
+	s.router.Fallthrough(config.WebRoot)
 
-	http.Handle("/", routes)
+	return s, nil
+}
+
+func (s *Server) AddRoute(format string, handler shared.IHandler, methods ...string) error {
+	return s.router.AddRoute(format, handler, methods...)
+}
+
+func (s *Server) AddRouteFunc(format string, hf func(shared.IRequest) (shared.IResponse, error), methods ...string) error {
+	return s.router.AddRouteFunc(format, hf, methods...)
+}
+
+func (s *Server) AddRoutePathFunc(format string, pathRequestFunc func(shared.IPathRequest) (shared.IResponse, error), methods ...string) error {
+	return s.router.AddRoutePathFunc(format, pathRequestFunc, methods...)
+}
+
+func (s *Server) Serve() error {
+	var err error
+
+	http.Handle("/", s.router)
 
 	// SERVE!
 
 	go func() {
-		err = http.ListenAndServe(config.BindAddress, nil)
+		err = http.ListenAndServe(s.config.BindAddress, nil)
 		if err != nil {
 			log.Println(err)
 		}
@@ -129,7 +174,7 @@ func Serve(config *ServerConfig) error {
 
 	log.Printf("=== SHUTDOWN INITIATED ===")
 
-	sessionStore.DumpSessions()
+	s.sessionStore.DumpSessions()
 
 	log.Println("=== SHUTDOWN COMPLETE ===")
 
